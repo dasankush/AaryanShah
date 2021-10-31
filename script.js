8f952f14c8ed0797292fda845e0fc76820457de8
DasAnkush <dasankush9284@gmail.com>
2021-10-31 10:50:44 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(document);
+let submitButton = document.getElementById('name');
+submitButton.addEventListener('click', function(){
+    submitButton.innerText = 'Aryan is my name.';
+})
